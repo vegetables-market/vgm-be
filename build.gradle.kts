560631e7494b900ce8241683ca3d01906eded8ad
yukid <yukiha617@gmail.com>
2025-12-08 18:46:44 +0900

    Flywayを導入し、初期テーブル作成処理を追加。

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     runtimeOnly("org.postgresql:postgresql")
 }
 
